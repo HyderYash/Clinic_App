5d1b658b353055611b441954304ac6efd88b74c0
HyderYash <yashsharma.karate@gmail.com>
2023-07-24 20:25:57 +0530

    Port change

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 require("dotenv").config();
 var cors = require("cors");
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 // Packages/Functions Import
